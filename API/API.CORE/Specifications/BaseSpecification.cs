a90a6412342451af190cd80e6876027417af563b
Ugur BOZKURT <ugur.bozkurt@ogr.deu.edu.tr>
2023-07-06 02:09:01 +0300

    Entityler Olusturuldu; Generic Repository olusturuldu;

---
+++ API/API.CORE/Specifications/BaseSpecification.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq.Expressions;
+
+namespace API.Core.Specifications
+{
+    public class BaseSpecification<T> : ISpecification<T>
+    {
+        public BaseSpecification(Expression<Func<T,bool>> criteria)
+        {
+            Criteria = criteria;
+        }
+        public Expression<Func<T, bool>> Criteria{ get; } 
+
+        public List<Expression<Func<T, object>>> Includes { get; } = new List<Expression<Func<T,object>>>();
+        protected void AddInclude(Expression<Func<T,object>> IncludeExpression)
+        {
+            Includes.Add(IncludeExpression);
+        }
+    }
+}
---
+++ API/API.CORE/Specifications/ISpecification.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq.Expressions;
+
+namespace API.Core.Specifications
+{
+    public interface ISpecification<T>
+    {
+        Expression<Func<T, bool>> Criteria { get; }
+        List<Expression<Func<T, object>>> Includes { get; }
+    }
+}
